b899a47eb5cfbad9ca104353bff8f322e6341905
Pepper <JohnnyDZ0707@gmail.com>
2025-05-03 15:02:14 -0700

    comments for camera

--- main/camera_2d.gd
+++ main/camera_2d.gd
@@ -7,7 +7,10 @@ extends Camera2D
 @onready var murder: Node2D = $"../Murder"
 var cameraTargetDict
 # Called when the node enters the scene tree for the first time.
+
+
 func _ready() -> void:
+	#sets list of targets for camera to lerp to 
 	cameraTargetDict = {
 	"EvilPlants" = evil.position,
 	"Murder" = murder.position
@@ -16,15 +19,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#lerp camera to target
 	global_position = global_position.lerp(cameraTargetDict[cameralerptarget], lerpspeed * delta)
 	pass
 
 
+#functions to swap out the targets
 func _on_arrow_to_murder_pressed() -> void:
 	cameralerptarget = "Murder"
 	pass
-
-
 func _on_arrow_to_evil_plants_pressed() -> void:
 	cameralerptarget = "EvilPlants"
 	pass # Replace with function body.
