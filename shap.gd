1bb7f77fdc081957ecf9f59de8b140d33b5ca676
mstaken <71734143+mstaken@users.noreply.github.com>
2025-05-03 12:08:24 -0700

    slight shap

--- shap.gd
+++ shap.gd
@@ -11,12 +11,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+# SEES WHETHER OR NOT MOUSE CURSOR IS INSIDE OF THE SHEEP
 func _on_mouse_entered():
 	mouse_inside = true
-
 func _on_mouse_exited():
 	mouse_inside = false
-	
+
+# SEES MOUSE CLICKS TO ALLOW DEATH
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT and mouse_inside:
 			print("I am kill")
