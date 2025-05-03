f7e2c4bbfe682d5f55275f1708b2a7d07984049d
mstaken <71734143+mstaken@users.noreply.github.com>
2025-05-03 12:05:21 -0700

    killable shap

--- shap.gd
+++ shap.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var mouse_inside : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_mouse_entered():
+	mouse_inside = true
+
+func _on_mouse_exited():
+	mouse_inside = false
+	
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT and mouse_inside:
+			print("I am kill")
