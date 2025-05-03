0968885335f42ec7f0f0b84e68983db5ec909d2e
Pepper <JohnnyDZ0707@gmail.com>
2025-05-03 16:20:03 -0700

    cursor follow

--- main/cursor_setter.gd
+++ main/cursor_setter.gd
@@ -1,14 +1,19 @@
 extends Node
 
 # Load the custom images for the mouse cursor.
-var arrow = load("res://arrow.png")
-var beam = load("res://beam.png")
-
-
-func _ready():
-	# Changes only the arrow shape of the cursor.
-	# This is similar to changing it in the project settings.
-	Input.set_custom_mouse_cursor(arrow)
-
-	# Changes a specific shape of the cursor (here, the I-beam shape).
-	Input.set_custom_mouse_cursor(beam, Input.CURSOR_IBEAM)
+#var arrow = load("res://assets/wateringCan.png")
+#
+#var beam = load("res://beam.png")
+#
+#
+#func _ready():
+	## Changes only the arrow shape of the cursor.
+	## This is similar to changing it in the project settings.
+	#Input.set_custom_mouse_cursor(arrow)
+#
+	## Changes a specific shape of the cursor (here, the I-beam shape).
+	#Input.set_custom_mouse_cursor(beam, Input.CURSOR_IBEAM)
+#func _process(delta: float) -> void:
+	#Input.set_custom_mouse_cursor(arrow)
+	
+# turns out godot cant do animated cursors natively yet
