9c452b6aeb2e3cb50184c4f00c46b24f8b90ce57
Pepper <JohnnyDZ0707@gmail.com>
2025-05-03 10:32:46 -0700

    camera scene switching

---
+++ main/camera_2d.gd
@@ -0,0 +1,30 @@
+extends Camera2D
+
+
+@export var lerpspeed: float = 10
+@onready var cameralerptarget: String = "EvilPlants"	
+@onready var evil: Node2D = $"../EvilPlants"
+@onready var murder: Node2D = $"../Murder"
+var cameraTargetDict
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	cameraTargetDict = {
+	"EvilPlants" = evil.position,
+	"Murder" = murder.position
+	}
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	global_position = global_position.lerp(cameraTargetDict[cameralerptarget], lerpspeed * delta)
+	pass
+
+
+func _on_arrow_to_murder_pressed() -> void:
+	cameralerptarget = "Murder"
+	pass
+
+
+func _on_arrow_to_evil_plants_pressed() -> void:
+	cameralerptarget = "EvilPlants"
+	pass # Replace with function body.
