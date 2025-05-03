501e6592899c9f963ec570a779c569f55bce22bf
mstaken <71734143+mstaken@users.noreply.github.com>
2025-05-03 16:45:54 -0700

    shaps move

---
+++ sheep_minigame.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var shap = preload("res://shap.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for i in range(0,9):
+		var new_shap = shap.instantiate()
+		add_child(new_shap)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
