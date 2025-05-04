f36d97d6b9546dffe06b907128826e35f6450e00
Pepper <JohnnyDZ0707@gmail.com>
2025-05-04 14:09:32 -0700

    fixed browser crashing when particle collision moves off screen by momentarily turning it off

--- main/camera_2d.gd
+++ main/camera_2d.gd
@@ -21,13 +21,21 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	#lerp camera to target
 	global_position = global_position.lerp(cameraTargetDict[cameralerptarget], lerpspeed * delta)
-	pass
+	if my_equal_approx(cameraTargetDict[cameralerptarget],global_position):
+		SignalManager.cameraPanFinished.emit()
+		#print("abbd")
 
 
 #functions to swap out the targets
 func _on_arrow_to_murder_pressed() -> void:
+	#print("a")
+	SignalManager.cameraPanStart.emit()
 	cameralerptarget = "Murder"
 	pass
 func _on_arrow_to_evil_plants_pressed() -> void:
+	SignalManager.cameraPanStart.emit()
 	cameralerptarget = "EvilPlants"
 	pass # Replace with function body.
+
+func my_equal_approx(a: Vector2, b:Vector2) -> bool:
+	return abs(a.x - b.x) < 0.5
--- signal_manager.gd
+++ signal_manager.gd
@@ -4,3 +4,6 @@ signal sheep_killed()
 signal blood_watered()
 signal seed_used()
 signal seed_gathered()
+
+signal cameraPanStart()
+signal cameraPanFinished()
