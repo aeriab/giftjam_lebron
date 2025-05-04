9cc5bc221f6f1bd9ac8c08496acd2e1a366d034b
BrendanA1622 <89553639+BrendanA1622@users.noreply.github.com>
2025-05-03 18:58:53 -0700

    adding daybloom to plant scene

---
+++ flower/daybloom_sprite.gd
@@ -0,0 +1,49 @@
+extends Sprite2D
+
+@export var MAX_OPACITY: float
+@export var MIN_OPACITY: float
+@export var FADE_IN_SPEED: float
+@export var FADE_OUT_SPEED: float
+
+var planted_tile: bool = false
+var being_hovered: bool = false
+var target_opacity: float = 0.0
+
+var current_opacity: float = 0.0
+
+func _process(delta):
+	if !planted_tile:
+		if being_hovered:
+			current_opacity = lerpf(current_opacity,target_opacity,FADE_IN_SPEED * delta)
+		else:
+			current_opacity = lerpf(current_opacity,target_opacity,FADE_OUT_SPEED * delta)
+	else:
+		current_opacity = lerpf(current_opacity,target_opacity,FADE_IN_SPEED * delta)
+	
+	#color.a = current_opacity
+
+func _on_mouse_entered():
+	if !planted_tile:
+		target_opacity = MAX_OPACITY
+	being_hovered = true
+
+
+func _on_mouse_exited():
+	if !planted_tile:
+		target_opacity = MIN_OPACITY
+	being_hovered = false
+
+
+func _on_gui_input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if !planted_tile:
+			target_opacity = MAX_OPACITY + randf_range(0.1, 0.2)
+			planted_tile = true
+			target_color = COLOR_PLANTED
+			SignalManager.blood_watered.emit()
+	
+	#if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
+		#if planted_tile:
+			#target_opacity = 0.0
+			#planted_tile = false
+			#target_color = COLOR_BLANK
--- flower/flower_scene.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-@onready var interactive_color_rect = $interactive_color_rect
-
-func glow():
-	interactive_color_rect.glow()
