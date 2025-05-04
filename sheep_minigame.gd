0f65181395765a6c77cfe2dfcaab58c04a06b4b6
mstaken <71734143+mstaken@users.noreply.github.com>
2025-05-03 17:51:46 -0700

    shaps spawn better

--- sheep_minigame.gd
+++ sheep_minigame.gd
@@ -4,8 +4,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for i in range(0,9):
+	for i in range(0,10):
 		var new_shap = shap.instantiate()
+		new_shap.global_position.x = (randf_range($"Pen Markers/topleft".position.x,$"Pen Markers/topright".position.x))
+		new_shap.global_position.y = (randf_range($"Pen Markers/topleft".position.y,$"Pen Markers/bottomleft".position.y))
+		print(new_shap.global_position)
 		$ShapHolder.add_child(new_shap)
 
 
