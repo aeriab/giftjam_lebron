9ef8ca43193d9b2ddc0954ac25906ce474e4880d
BrendanA1622 <89553639+BrendanA1622@users.noreply.github.com>
2025-05-06 01:53:20 -0700

    I think it runs !!!!

--- main/audio_stream_player_2d.gd
+++ main/audio_stream_player_2d.gd
@@ -4,6 +4,7 @@ var Syncstream
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Syncstream = preload("res://main/backgroundMusicStream.tres")
+	play()
 
 var curr_evil = 0
 
